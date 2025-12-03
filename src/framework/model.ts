c3900c5d8e7eaf8e749cdd9e6908bfbe2ef38c95
David Kossoglyad <david@logoui.co.il>
2025-12-03 23:07:21 +0200

    feat: introduce the Model decorator

--- src/framework/model.ts
+++ src/framework/model.ts
@@ -1,12 +1,39 @@
-import { ValidationError } from "class-validator";
-import { makeAutoObservable } from "mobx";
+import { validateOrReject, ValidationError } from "class-validator";
+import { makeAutoObservable, observe, IObjectDidChange } from "mobx";
 
-export interface IModel {
-  errors: ValidationError[];
-}
+export function Model() {
+  return function <T extends { new (...args: any[]): {} }>(constructor: T) {
+    // Store the original constructor 
+    const original = constructor;
 
-export abstract class Model {
-  public readonly errors: ValidationError[] = [];
+    // Create new constructor function
+    const newConstructor: any = function (...args: any[]) {
+      const instance = new original(...args);
+      
+      // Add observe logic to constructor
+      makeAutoObservable(instance);
+      observe(instance, (change: IObjectDidChange) => {
+        if (change.type === "update" && change.newValue !== change.oldValue) {
+          (instance as any).validate();
+        }
+      });
+      
+      return instance;
+    }
 
-  
+    // Copy prototype
+    newConstructor.prototype = original.prototype;
+
+    // Add validate method to prototype 
+    newConstructor.prototype.validate = function() {
+      this.errors.splice(0, this.errors.length);
+      validateOrReject(this).catch((errors: ValidationError[]) => {
+        this.errors.push(...errors);
+      });
+    }
+
+    newConstructor.prototype.errors = [];
+
+    return newConstructor;
+  }
 }
